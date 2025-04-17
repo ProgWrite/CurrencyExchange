c7da21261f778524bd0c5b586b8cac24561f1dc2
ProgWrite <dimkakuz123@gmail.com>
2025-04-17 20:05:11 +0300

    Initial commit

---
+++ src/main/java/CurrencyExchange/entity/Currencies.java
@@ -0,0 +1,72 @@
+package CurrencyExchange.entity;
+
+import java.util.Objects;
+
+public class Currencies {
+    private int id;
+    private String code;
+    private String fullName;
+    private String sign;
+
+    public Currencies(int id, String code, String fullName, String sign) {
+        this.id = id;
+        this.code = code;
+        this.fullName = fullName;
+        this.sign = sign;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Currencies that = (Currencies) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Currencies{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", sign='" + sign + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/CurrencyExchange/util/PropertiesUtil.java
@@ -0,0 +1,6 @@
+package CurrencyExchange.util;
+
+public class PropertiesUtil {
+
+
+}
